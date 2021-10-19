edffb57aea81f24f1a5639c73e9bb77a0d747337
+ <+>
2021-10-18 23:36:34 -0500

    commit tree 94995e98d207e1a728b92b84cb1a955e76833c63

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+    include Dance
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    extend MetaDancing
+    extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
